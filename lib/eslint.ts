3f011d8b44bf24e95cbe7d7da122292f3b32a936
jinzeze <jinzeze@medlinker.com>
2022-07-05 18:34:14 +0800

    feat: 修改eslint规则

--- lib/eslint.ts
+++ lib/eslint.ts
@@ -2,7 +2,6 @@ module.exports = {
   // https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21
   extends: [
     'airbnb',
-    'airbnb/hooks',
     'prettier',
     'plugin:compat/recommended',
     'plugin:react/recommended',
@@ -33,12 +32,13 @@ module.exports = {
   },
   rules: {
     'no-var': 2, // 禁止使用 var
+    'arrow-body-style': 0,
+    'react/no-unused-prop-types': 1,
+    'import/order': 1,
     'import/no-unresolved': 0,
-    // https://eslint.org/docs/rules/no-use-before-define
     'no-use-before-define': 0,
     '@typescript-eslint/no-use-before-define': 0,
     'no-plusplus': 0,
-    // https://github.com/import-js/eslint-plugin-import/blob/v2.25.3/docs/rules/prefer-default-export.md
     'import/prefer-default-export': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
@@ -47,7 +47,6 @@ module.exports = {
     'import/extensions': 0,
     'react/display-name': 0,
     'react/jsx-filename-extension': 0,
-    // https://github.com/typescript-eslint/typescript-eslint/blob/v2.34.0/packages/eslint-plugin/docs/rules/naming-convention.md
     '@typescript-eslint/naming-convention': [
       1,
       {
