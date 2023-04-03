2c02c769d5eb9d255c3602101589a46697b2dacc
xz <17402175+xz-77@users.noreply.github.com>
2023-04-03 11:17:28 +0800

    feat: 完善prettier、stylelint规则

--- lib/stylelint.ts
+++ lib/stylelint.ts
@@ -1,17 +1,20 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-rational-order',
-    'stylelint-config-prettier',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
+  customSyntax: 'postcss-scss',
   plugins: ['stylelint-declaration-block-no-ignored-properties'],
   rules: {
-    'comment-empty-line-before': null,
-    'function-name-case': ['lower'],
-    'no-invalid-double-slash-comments': null,
-    'no-descending-specificity': null,
-    'no-invalid-position-at-import-rule': null,
-    'declaration-empty-line-before': null,
-    'at-rule-no-unknown': null,
+    'plugin/declaration-block-no-ignored-properties': true, //不允许由于同一规则中的另一个属性值而被忽略的属性值 如：a { display: inline; width: 100px; }
+    'function-name-case': ['lower'], // 为函数名称指定小写或大写。
+    'color-function-notation': 'legacy', //为颜色函数指定现代或传统符号。 如：legacy
+    'no-descending-specificity': null,// 不允许较低特异性的选择器出现在覆盖较高特异性的选择器之后
+    'function-url-quotes': 'always',// 要求url使用引号
+    'selector-attribute-quotes': 'always',// 选择器属性携带引号
+    'selector-class-pattern': [
+      '^([a-z][a-z0-9]*(-[a-z0-9]+)*|[a-z][a-zA-Z0-9]+)$',
+      {
+        message: 'Expected class selector to be kebab-case or lowerCamelCase',
+      },
+    ],// 限制属性命名规则
   },
+  ignoreFiles: ['node_modules'],
 };
